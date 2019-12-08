de016e8ced2679d46fa89b7921e2ac4762568810
wahyudevi <54773394+wahyudevi@users.noreply.github.com>
2019-12-08 10:06:53 +0700

    Room & Reservation

---
+++ PROJECTUASPBO/java_Hotel_System/src/java_hotel_system/RegularRoom.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_hotel_system;
+
+/**
+ *
+ * @author hokyt
+ */
+public class RegularRoom extends abstrak{
+
+    @Override
+    public String getType() {
+       return "String";
+    }
+    
+}
---
+++ PROJECTUASPBO/java_Hotel_System/src/java_hotel_system/SuperRoom.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_hotel_system;
+
+/**
+ *
+ * @author hokyt
+ */
+public class SuperRoom extends abstrak{
+
+    @Override
+    public String getType() {
+       return "String";
+    }
+
+    
+    
+    
+    
+}
